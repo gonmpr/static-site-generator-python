1b35c61fb1f7fa8f2dca1629d4cb9734ec0abf17
gonmpr <gmporrorosas@gmail.com>
2024-12-27 20:38:53 -0300

    Add textnode.py

--- public/src/main.py
+++ public/src/main.py
@@ -1,6 +1,11 @@
+from textnode import TextType, TextNode
 
 def main():
-    print("hello world")
+    test_node = TextNode("this is text bold","bold")
+    test_node2 = TextNode("this text bold","bold")
+
+
+    print(test_node == test_node2)
 
 
 
